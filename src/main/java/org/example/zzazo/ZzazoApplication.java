d6f7861a47789b28f6fccce7a760171797a97912
Nanajun22 <skaudwns3@gmail.com>
2026-07-01 17:18:42 +0900

    feat: 초기 프로젝트 세팅

---
+++ src/main/java/org/example/zzazo/ZzazoApplication.java
@@ -0,0 +1,13 @@
+package org.example.zzazo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZzazoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZzazoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/zzazo/ZzazoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.zzazo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZzazoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

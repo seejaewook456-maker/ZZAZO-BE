77da3ed6f2546fdd190ddf05e038c3d5be6b7b11
Nanajun22 <skaudwns3@gmail.com>
2026-07-03 20:18:17 +0900

    refactor: 헬스체크, auth 엔드포인트 형식 수정

--- src/main/java/org/example/zzazo/global/controller/HealthController.java
+++ src/main/java/org/example/zzazo/global/controller/HealthController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/v1")
 // 서버 상태 확인 API Controller
 public class HealthController implements HealthControllerDocs {
 

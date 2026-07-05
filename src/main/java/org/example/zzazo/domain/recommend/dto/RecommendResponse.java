9715467c62de11e79a45fc4f5f7ab95a7474fe23
Nanajun22 <skaudwns3@gmail.com>
2026-07-05 21:06:19 +0900

    feat:시간표 추천 API 및 DTO 구현

--- src/main/java/org/example/zzazo/domain/recommend/dto/RecommendResponse.java
+++ src/main/java/org/example/zzazo/domain/recommend/dto/RecommendResponse.java
@@ -1,4 +1,38 @@
 package org.example.zzazo.domain.recommend.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import org.example.zzazo.domain.lecture.domain.LectureClassification;
+import org.example.zzazo.global.common.Week;
+
+import java.time.LocalTime;
+import java.util.List;
+
 public class RecommendResponse {
+    public record RecommendResult(
+            int totalCredits,
+            Week[] satisfiedFreeDays,
+            List<lecture> timetables
+    ) {
+
+    }
+
+    public record lecture(
+            Long lectureId,
+            String lectureName,
+            int credit,
+            LectureClassification category,
+            List<LectureTime> lectureTime
+            ) {
+        public record LectureTime(
+                Week day,
+                @JsonFormat(pattern = "HH:mm")
+                LocalTime startTime,
+                @JsonFormat(pattern = "HH:mm")
+                LocalTime endTime
+        ) {
+
+        }
+    }
+
+
 }

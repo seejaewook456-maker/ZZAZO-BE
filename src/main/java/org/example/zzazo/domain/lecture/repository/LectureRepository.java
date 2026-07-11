5a0cf3d1f5ccf18c5d07dd2bc9ef4495c76f8b53
Nanajun22 <skaudwns3@gmail.com>
2026-07-11 16:47:06 +0900

    feat: 과목목록 조회 벌크 쿼리 추가

--- src/main/java/org/example/zzazo/domain/lecture/repository/LectureRepository.java
+++ src/main/java/org/example/zzazo/domain/lecture/repository/LectureRepository.java
@@ -4,6 +4,8 @@ import org.example.zzazo.domain.lecture.domain.LiberalCategory;
 import org.example.zzazo.domain.lecture.entity.Lecture;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -12,4 +14,8 @@ public interface LectureRepository extends JpaRepository<Lecture, Long> {
     //교양 과목목록 조회
     @EntityGraph(attributePaths = {"lectureSchedules"})
     List<Lecture> findAllByLiberalCategoryAndSemester(LiberalCategory liberalCategory,int semester);
+
+    //id로 과목목록 조회
+    @Query("select distinct l from Lecture l left join fetch l.lectureSchedules where l.id in :ids")
+    List<Lecture> findAllByIdInWithSchedules(@Param("ids") List<Long> ids);
 }
